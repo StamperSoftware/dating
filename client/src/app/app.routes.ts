7973f7311834bbb9dd14d4a522f3c0ec4f78a19d
StamperSoftware <StamperSoftware@gmail.com>
2024-08-18 13:39:33 -0400

    Added initial error handling

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -6,6 +6,8 @@ import {MemberDetailComponent} from "./members/detail/detail.component";
 import {ListsComponent} from "./lists/lists.component";
 import {MessagesComponent} from "./messages/messages.component";
 import {authGuard} from "../../guards/auth.guard";
+import {ServerErrorComponent} from "./errors/server-error/server-error.component";
+import {NotFoundComponent} from "./errors/not-found/not-found.component";
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
@@ -21,5 +23,7 @@ export const routes: Routes = [
             {path: 'messages', component: MessagesComponent},
         ]
     },
-    {path: '**', component: HomeComponent, pathMatch: 'full'},
+    {path: 'not-found', component: NotFoundComponent},
+    {path: 'server-error', component: ServerErrorComponent},
+    {path: '**', component: NotFoundComponent, pathMatch: 'full'},
 ];
