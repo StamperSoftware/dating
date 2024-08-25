12b75196056442700297ba1b2ed7c438be71b612
StamperSoftware <StamperSoftware@gmail.com>
2024-08-25 11:14:52 -0400

    Added automapper and user repository

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -15,24 +15,23 @@ public class AccountController(DataContext context, ITokenService tokenService) 
     public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
         if (await UserExists(registerDto.UserName)) return BadRequest("UserName has been taken.");
-
-        using var hmac = new HMACSHA512();
-        var user = new AppUser()
-        {
-            UserName = registerDto.UserName.ToLower(),
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-            PasswordSalt = hmac.Key,
-        };
-
-        context.Users.Add(user);
-        await context.SaveChangesAsync();
-        return new UserDto()
-        {
-            UserName = registerDto.UserName, 
-            Token = tokenService.CreateToken(user)
-        };
-    ;
-}
+        return Ok();
+        // using var hmac = new HMACSHA512();
+        // var user = new AppUser()
+        // {
+        //     UserName = registerDto.UserName.ToLower(),
+        //     PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+        //     PasswordSalt = hmac.Key,
+        // };
+        //
+        // context.Users.Add(user);
+        // await context.SaveChangesAsync();
+        // return new UserDto()
+        // {
+        //     UserName = registerDto.UserName, 
+        //     Token = tokenService.CreateToken(user)
+        // };
+    }
 
     [HttpPost("login")]
     public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
